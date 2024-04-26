18ac3b3a0175e67ee48a24230bf3b1997f826831
Nelson Quintanilla <nfquinta@gmail.com>
2024-04-26 09:02:37 +0800

    Set up navigation and add permission to manifest

--- app/src/main/java/com/nelsonquintanilla/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/nelsonquintanilla/cryptocurrencyapp/presentation/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.nelsonquintanilla.cryptocurrencyapp.common.Constants.PARAM_COIN_ID
+import com.nelsonquintanilla.cryptocurrencyapp.presentation.coindetail.CoinDetailScreen
+import com.nelsonquintanilla.cryptocurrencyapp.presentation.coinlist.CoinListScreen
 import com.nelsonquintanilla.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +27,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{${PARAM_COIN_ID}}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptocurrencyAppTheme {
-        Greeting("Android")
     }
 }
